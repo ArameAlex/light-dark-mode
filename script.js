f4415775438a92f5427ba4baab52c0009d4631a0
ArameAlex <alexanderianarame@gmail.com>
2024-12-25 16:21:27 +0330

    new background

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ const toggleButton = document.getElementById("mode-toggle");
 const body = document.body;
 
 function ligtOrDark(){
+  const random = Math.random()
   // if it was dark mode
   if (body.classList.contains("dark-mode")){
     // button color and text
     toggleButton.textContent = "Switch to Dark Mode";
     toggleButton.classList = "dark-mode";
-    // body color
-    body.classList = "light-mode";
+    if (random < 0.5){
+      body.classList = "light-mode back1";
+    } else {
+      body.classList = "light-mode back2";
+    }
+    
   } else if (body.classList.contains("light-mode")){
     // button color and text
     toggleButton.textContent = "Switch to Light Mode";
     toggleButton.classList = "light-mode";
-    // body color
-    body.classList = "dark-mode";
+    if (random < 0.5){
+      body.classList = "dark-mode back3";
+    } else {
+      body.classList = "dark-mode back4";
+    }
   }
 }
